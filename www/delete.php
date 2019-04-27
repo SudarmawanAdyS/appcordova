e919e91615d459f0e9f49c94c5eb3558a1d3d1be
SudarmawanAdyS <48430387+SudarmawanAdyS@users.noreply.github.com>
2019-04-27 21:22:12 +0700

    Update delete.php

--- www/delete.php
+++ www/delete.php
@@ -2,7 +2,7 @@
  include "db.php";
  if(isset($_GET['id_baak']))
  {
- $id=$_GET['id_baak'];
+ $id_baak=$_GET['id_baak'];
  $q=mysqli_query($con,"delete from `baak` where `id_baak`='$id_baak'");
  if($q)
  echo "success";
