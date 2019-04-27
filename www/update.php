5fadbcd0d4ef7437a5e1b8ed9adfd91727eb1efa
SudarmawanAdyS <48430387+SudarmawanAdyS@users.noreply.github.com>
2019-04-27 21:25:19 +0700

    Update update.php

--- www/update.php
+++ www/update.php
@@ -6,7 +6,8 @@
  $nama_baak=$_POST['nama_baak'];
  $email=$_POST['email'];
  $telp=$_POST['telp'];
-  $alamat=$_POST['alamat'];
+ $alamat=$_POST['alamat'];
+ 
  $q=mysqli_query($con,"UPDATE `baak` SET `id_baak`='$id_baak',`nama_baak`='$nama_baak',`email`='$email',`telp`='$telp',`alamat`='$alamat' where `id_baak`='$id_baak'");
  if($q)
  echo "success";
