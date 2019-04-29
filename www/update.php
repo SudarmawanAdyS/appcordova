c4368c06815732d38d2cf72fa5811eca5fc38e2e
SudarmawanAdyS <48430387+SudarmawanAdyS@users.noreply.github.com>
2019-04-29 12:30:13 +0700

    Update update.php

--- www/update.php
+++ www/update.php
@@ -2,13 +2,13 @@
  include "db.php";
  if(isset($_POST['update']))
  {
- $id_baak=$_POST['id_baak'];
- $nama_baak=$_POST['nama_baak'];
- $email=$_POST['email'];
+ $nip=$_POST['nip'];
+ $nama=$_POST['nama'];
+
  $telp=$_POST['telp'];
- $alamat=$_POST['alamat'];
- 
- $q=mysqli_query($con,"UPDATE `baak` SET `id_baak`='$id_baak',`nama_baak`='$nama_baak',`email`='$email',`telp`='$telp',`alamat`='$alamat' where `id_baak`='$id_baak'");
+ $email=$_POST['email'];
+  $alamat=$_POST['alamat'];
+ $q=mysqli_query($con,"UPDATE `progdi` SET `nip`='$nip',`nama`='$nama',`telp`='$telp',`email`='$email',`alamat`='$alamat' where `nip`='$nip'");
  if($q)
  echo "success";
  else
